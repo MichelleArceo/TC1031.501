5aaba402f44d3c270bef05a0d6701a38a9cf988e
MichelleArceo <71092063+MichelleArceo@users.noreply.github.com>
2020-11-23 22:08:39 -0600

    Update main.cpp

--- Actividad4.3/main.cpp
+++ Actividad4.3/main.cpp
@@ -1,7 +1,6 @@
 /*
 Programa para analizar logs de intentos fallidos de ingreso (bitacora3.txt).
 
-    Matías Kochlowski – a01625364
     Esteban Sánchez - a01251440
     Michelle Arceo - a01625268
 
