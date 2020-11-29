78002f2a5aa5f0a627930c042524a707dd690d9f
MichelleArceo <71092063+MichelleArceo@users.noreply.github.com>
2020-11-29 01:47:29 -0600

    Add files via upload

---
+++ Actividad5.2/main.cpp
@@ -0,0 +1,7 @@
+/*
+Programa para analizar logs de intentos fallidos de ingreso (bitacora3.txt).
+    Esteban Sánchez - a01251440
+    Michelle Arceo - a01625268
+Creación: Sábado, 28 de Noviembre
+*/
+
